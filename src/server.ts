591170d1bf71ea1cc5ea3991590ac243158cc4a5
NEYAZ N <neyaznafiz@outlok.com>
2023-05-29 02:59:00 +0600

    express, database, cors, parser, env config setup done

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import app from "./app";
+import config from "./config/config";
+
+async function main() {
+  try {
+    await mongoose.connect(config.database_url as string);
+    console.log(`✅ Database connected successful`);
+    app.listen(config.port, () => {
+      console.log(`✅ UMP Application listening on port ${config.port}`);
+    });
+  } catch (err) {
+    console.log(`🔻Failed to connect database !`, err);
+  }
+}
+
+main();

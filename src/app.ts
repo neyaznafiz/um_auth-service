03ad8fb41eb0c4117bd7624cd04e642e6a8cbf62
NEYAZ N <neyaznafiz@outlok.com>
2023-05-30 01:08:19 +0600

    eslint prettier and husky setup done

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,11 @@ import cors from 'cors';
 
 const app: Application = express();
 
-        app.use(cors());
+          app.use(cors());
 
 // parser
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req: Request, res: Response) => {
   res.send('University Management Server Is Running...!');

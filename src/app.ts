b8fb90b9845835819fedbad811f77d1763faf9c9
NEYAZ N <neyaznafiz@outlok.com>
2023-05-30 01:05:34 +0600

    eslint prettier and husky setup done

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,16 @@
-import express, { Application, Request, Response } from "express";
-import cors from "cors";
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
 
 const app: Application = express();
 
-app.use(cors());
+        app.use(cors());
 
 // parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("University Management Server Is Running...!");
+app.get('/', (req: Request, res: Response) => {
+  res.send('University Management Server Is Running...!');
 });
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config/config";
+import mongoose from 'mongoose';
+import app from './app';
+import config from './config/config';
 
 async function main() {
   try {

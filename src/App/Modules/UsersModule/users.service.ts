cc496dc5fc99fbabe61741474fa8600de42f2e64
NEYAZ N <neyaznafiz@outlok.com>
2023-05-31 01:55:27 +0600

    user id genered function and create user function added

---
+++ src/App/Modules/UsersModule/users.service.ts
@@ -0,0 +1,25 @@
+import config from '../../../Config/config';
+import { TUser } from './users.interface';
+import { User } from './users.model';
+import { generateUserId } from './users.utils';
+
+const createUser = async (user: TUser): Promise<TUser | null> => {
+  // auto generatedincremental id
+  const id = await generateUserId();
+
+  user.id = id;
+  // default password
+  if (!user.password) {
+    user.password = config.default_user_pass as string;
+  }
+
+  const createdUser = await User.create(user);
+  if (!createdUser) {
+    throw new Error('Failed to create user..!');
+  }
+  return createdUser;
+};
+
+export default {
+  createUser,
+};

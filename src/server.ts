71bd3af66c2294546b9f4176debe02becc1024e3
NEYAZ N <neyaznafiz@outlok.com>
2023-06-02 01:39:44 +0600

    winston logger implimented

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,17 @@
 import mongoose from 'mongoose';
 import app from './App/app';
 import config from './Config/config';
+import logger from './Global/logger';
 
 async function main() {
   try {
     await mongoose.connect(config.database_url as string);
-    console.log(`✅ Database connected successful`);
+    logger.info(`✅ Database connected successful`);
     app.listen(config.port, () => {
-      console.log(`✅ UMP Application listening on port ${config.port}`);
+      logger.info(`✅ UMP Application listening on port ${config.port}`);
     });
   } catch (err) {
-    console.log(`🔻Failed to connect database !`, err);
+    logger.error(`🔻Failed to connect database !`, err);
   }
 }
 
